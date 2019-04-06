9173ee9e07073095f6f47eb2f510e99dfd2f9bab
mdb <mdb@mdblabs.com>
2019-04-06 14:20:15 +0200

    Corregida errata

--- openboxes-run.sh
+++ openboxes-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-pt-get update -yq \
-&& apt-get install curl gnupg -yq \
-&& curl -sL https://deb.nodesource.com/setup_8.x | bash \
-&& apt-get install nodejs -yq
+apt-get update -yq 
+apt-get install curl gnupg -yq 
+curl -sL https://deb.nodesource.com/setup_8.x | bash 
+apt-get install nodejs -yq
+
 
 if [ ! -f /app/web-app/WEB-INF/applicationContext.xml ]; then
    echo "Forcing grails upgrade"
