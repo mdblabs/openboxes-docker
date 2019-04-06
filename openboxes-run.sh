e3038a13859f4114c68aa30339533c90956c3f2c
mdb <mdb@mdblabs.com>
2019-04-06 15:16:27 +0200

    npm install

--- openboxes-run.sh
+++ openboxes-run.sh
@@ -4,6 +4,7 @@ apt-get install curl gnupg -yq
 curl -sL https://deb.nodesource.com/setup_8.x | bash 
 apt-get install nodejs -yq
 
+npm install
 
 if [ ! -f /app/web-app/WEB-INF/applicationContext.xml ]; then
    echo "Forcing grails upgrade"
